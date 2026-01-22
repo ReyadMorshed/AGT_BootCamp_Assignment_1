ad0d48f99db6984cdddd6c082d6d1d8d1a90d296
ReyadMorshed <46967229+ReyadMorshed@users.noreply.github.com>
2026-01-22 14:14:39 +0600

    error

--- src/utilities/utils/dashboard/dashboardUtils.ts
+++ src/utilities/utils/dashboard/dashboardUtils.ts
@@ -1,18 +1,6 @@
 import { Page, Locator } from "@playwright/test";
-import { DashboardPage } from "../../../pages/dashboard/dashboard.page";
 import { performClick } from "../../actions/elementActions";
-
-export async function navigateToLeave(
-  page: Page,
-  dashboard: DashboardPage,
-): Promise<void> {
-  dashboard = new DashboardPage(page);
-  await performClick(dashboard.LeaveLink, "Leave Link", page);
-}
-
-import { Page, Locator } from "@playwright/test";
-import { performClick } from "../../actions/elementActions";
-import { DashboardPage } from "../../../../pages/Dashboard/dashboard.page";
+import { DashboardPage } from "../../../pages/Dashboard/dashboard.page";
 
 export async function navigateToLeave(
   page: Page,
