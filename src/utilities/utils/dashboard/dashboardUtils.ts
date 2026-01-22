234727e0a22a80c53451d0510afd8ade20792806
ReyadMorshed <46967229+ReyadMorshed@users.noreply.github.com>
2026-01-22 14:07:41 +0600

    fixing error

---
+++ src/utilities/utils/dashboard/dashboardUtils.ts
@@ -0,0 +1,11 @@
+import { Page, Locator } from "@playwright/test";
+import { performClick } from "../../actions/elementActions";
+import { DashboardPage } from "../../../../pages/Dashboard/dashboard.page";
+
+export async function navigateToLeave(
+  page: Page,
+  dashboard: DashboardPage,
+): Promise<void> {
+  dashboard = new DashboardPage(page);
+  await performClick(dashboard.LeaveLink, "Leave Link", page);
+}
