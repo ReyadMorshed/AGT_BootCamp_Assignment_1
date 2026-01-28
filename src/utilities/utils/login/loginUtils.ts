3c5ff8d562b516facf8bdf011f2da6d27989d631
ReyadMorshed <46967229+ReyadMorshed@users.noreply.github.com>
2026-01-28 20:46:35 +0600

    remove validation from login test

--- src/utilities/utils/login/loginUtils.ts
+++ src/utilities/utils/login/loginUtils.ts
@@ -3,8 +3,10 @@ import {
   performFill,
   performClick,
   performGoto,
+  waitForVisible,
 } from "../../actions/elementActions";
 import { LoginPage } from "../../../pages/login/login.page";
+import { DashboardPage } from "../../../pages/Dashboard/dashboard.page";
 
 /**
  * Logs into the OrangeHRM application using provided credentials and URL.
@@ -13,6 +15,7 @@ import { LoginPage } from "../../../pages/login/login.page";
  * @param username - The username to use for login.
  * @param password - The password to use for login.
  */
+let dashboardPage: DashboardPage;
 export async function loginToOrangeHRM(
   page: Page,
   url: string,
@@ -21,9 +24,16 @@ export async function loginToOrangeHRM(
   password: string,
 ): Promise<void> {
   loginPage = new LoginPage(page);
+  dashboardPage = new DashboardPage(page);
   await performGoto(page, url, "OrangeHRM Login Page");
   await page.waitForTimeout(2000); // Wait for 30 seconds to ensure page is fully loaded
   await performFill(loginPage.usernameField, username, "Username field", page);
   await performFill(loginPage.passwordField, password, "Password field", page);
   await performClick(loginPage.loginButton, "Login button", page);
+  await waitForVisible(dashboardPage.dashboardText, "Dashboard Text", 20000);
+  await waitForVisible(
+    dashboardPage.timeAtWorkText,
+    "Time at Work Text",
+    20000,
+  );
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,8 @@
 import { expect } from "@playwright/test";
 import { test } from "../src/fixture/sessionLogin"; // <-- Use the custom test
 import { DashboardPage } from "../src/pages/Dashboard/dashboard.page";
+import { waitForVisible } from "../src/utilities/actions/elementActions";
 
 test.describe("OrangeHRM Login Tests", () => {
-  test("login using sessionLogin fixture", async ({ loggedInPage }) => {
-    const dashboardPage = new DashboardPage(loggedInPage);
-
-    await expect(dashboardPage.dashboardText).toBeVisible({ timeout: 5000 });
-    await expect(dashboardPage.timeAtWorkText).toBeVisible({ timeout: 5000 });
-  });
+  test("login using sessionLogin fixture", async ({ loggedInPage }) => {});
 });
